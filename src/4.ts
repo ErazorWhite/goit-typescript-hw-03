f1860cd9db1b99830a032abc24dd46c2c30928ed
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2024-03-19 23:37:34 +0200

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {
+    this.door = false;
+  }
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(protected key: Key) {
+    super(key);
+  }
+  public openDoor(key: Key) {
+    const houseDoorKey = this.key.getSignature();
+    const personKey = key.getSignature();
+    if (houseDoorKey === personKey) {
+      this.door = true;
+    } else {
+      console.log("Wrong key!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
